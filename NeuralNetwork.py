ffd5405e48ce29142bca2c918136ab77881d1950
Trung Nguyen <trungthanh.nguyen@team.enouvo.com>
2024-03-22 21:41:18 -0500

    Add momentum optimizer; add comments; change README

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -5,7 +5,7 @@ from sklearn.model_selection import train_test_split
 
 
 class NeuralNetwork():
-    def __init__(self, input_layer, hidden_layer, output_layer, activation='sigmoid'):
+    def __init__(self, input_layer, hidden_layer, output_layer, activation='sigmoid', beta=0.9):
         self.input_layer = input_layer
         self.hidden_layer = hidden_layer
         self.output_layer = output_layer
@@ -15,21 +15,24 @@ class NeuralNetwork():
             self.activation = self.sigmoid
         else:
             self.activation = self.tanh
-        # Save all weights
+        # Initialize weights and biases at each layer
         self.parameters = {
             "W1": np.random.randn(hidden_layer, input_layer) * np.sqrt(1./input_layer),
-            "bias1": np.zeros((hidden_layer, 1)) * np.sqrt(1./input_layer),
+            "bias_at_hidden_layer": np.zeros((hidden_layer, 1)) * np.sqrt(1./input_layer),
             "W2": np.random.randn(output_layer, hidden_layer) * np.sqrt(1./hidden_layer),
-            "bias2": np.zeros((output_layer, 1)) * np.sqrt(1./hidden_layer)
+            "bias_at_output_layer": np.zeros((output_layer, 1)) * np.sqrt(1./hidden_layer)
         }
+        # cache to save values during forward pass to use in backward pass
         self.cache = {}
 
-    def relu(self, x, derivative=False):
-        if derivative:
-            x = np.where(x < 0, 0, x)
-            x = np.where(x >= 0, 1, x)
-            return x
-        return np.maximum(0, x)
+        # Initialize momentum terms for each parameter
+        self.momentum = {
+            "W1": np.zeros((hidden_layer, input_layer)),
+            "bias_at_hidden_layer": np.zeros((hidden_layer, 1)),
+            "W2": np.zeros((output_layer, hidden_layer)),
+            "bias_at_output_layer": np.zeros((output_layer, 1))
+        }
+        self.beta = beta  # Momentum hyperparameter
 
     def sigmoid(self, x, derivative=False):
         if derivative:
@@ -41,24 +44,34 @@ class NeuralNetwork():
             return 1 - np.tanh(x)**2
         return np.tanh(x)
 
-    def forward_pass(self, x):
-        # y = activision(wX + b)
+    def relu(self, x, derivative=False):
+        if derivative:
+            x = np.where(x < 0, 0, x)
+            x = np.where(x >= 0, 1, x)
+            return x
+        return np.maximum(0, x)
 
-        self.cache["input_neurons"] = x
+    def forward_pass(self, X):
 
+        self.cache["input_neurons"] = X
+
+        # net = wX + b
         self.cache["net_from_inputs"] = np.matmul(
-            self.parameters["W1"], self.cache["input_neurons"].T) + self.parameters["bias1"]
+            self.parameters["W1"], self.cache["input_neurons"].T) + self.parameters["bias_at_hidden_layer"]
+        # output = activision(wX + b)
         self.cache["hidden_layer_neurons"] = self.activation(
             self.cache["net_from_inputs"])
+        # net = wX + b
         self.cache["net_from_hidden_layer"] = np.matmul(
-            self.parameters["W2"], self.cache["hidden_layer_neurons"]) + self.parameters["bias2"]
+            self.parameters["W2"], self.cache["hidden_layer_neurons"]) + self.parameters["bias_at_output_layer"]
+        # output = activision(wX + b)
         self.cache["output_layer_neurons"] = self.activation(
             self.cache["net_from_hidden_layer"])
         return self.cache["output_layer_neurons"]
 
-    def backward_pass(self, y_true, learning_rate=0.01):
-        # δ(output) = (y - y_true) * derivative(activation)
-        delta_output = (y_true.T - self.cache["output_layer_neurons"]) * self.activation(
+    def backward_pass(self, y_original, learning_rate=0.01):
+        # δ(output) = (t - o) * derivative(activation)
+        delta_output = (y_original.T - self.cache["output_layer_neurons"]) * self.activation(
             self.cache["net_from_hidden_layer"], derivative=True)
 
         # ∆(W2) = learning_rate * (delta(output) * hidden_layer_neurons)
@@ -79,56 +92,75 @@ class NeuralNetwork():
 
         # Update parameters using gradients and learning rate
         self.parameters["W2"] += delta_W2
-        self.parameters["bias2"] += delta_bias2
+        self.parameters["bias_at_output_layer"] += delta_bias2
         self.parameters["W1"] += delta_W1
-        self.parameters["bias1"] += delta_bias1
+        self.parameters["bias_at_hidden_layer"] += delta_bias1
 
-    def accuracy(self, y, output):
-        y_pred = np.argmax(output.T, axis=-1)
-        y_true = np.argmax(y, axis=-1)
-        return np.mean(y_pred == y_true)
+        self.momentum_optimizer(delta_W2, delta_bias2, delta_W1, delta_bias1)
+
+        # Update parameters using gradients and momentum
+        self.parameters["W2"] += self.momentum["W2"]
+        self.parameters["bias_at_output_layer"] += self.momentum["bias_at_output_layer"]
+        self.parameters["W1"] += self.momentum["W1"]
+        self.parameters["bias_at_hidden_layer"] += self.momentum["bias_at_hidden_layer"]
+
+    def momentum_optimizer(self, delta_W2, delta_bias2, delta_W1, delta_bias1):
+        # Update momentum
+        self.momentum["W2"] = self.beta * \
+            self.momentum["W2"] + (1 - self.beta) * delta_W2
+        self.momentum["bias_at_output_layer"] = self.beta * \
+            self.momentum["bias_at_output_layer"] + \
+            (1 - self.beta) * delta_bias2
+        self.momentum["W1"] = self.beta * \
+            self.momentum["W1"] + (1 - self.beta) * delta_W1
+        self.momentum["bias_at_hidden_layer"] = self.beta * \
+            self.momentum["bias_at_hidden_layer"] + \
+            (1 - self.beta) * delta_bias1
+
+    def get_accuracy(self, y, output):
+        y_predicted = np.argmax(output.T, axis=-1)
+        y_original = np.argmax(y, axis=-1)
+        return np.mean(y_predicted == y_original)
 
     def train(self, x_train, y_train, x_test, y_test, epochs=10,
-              batch_size=64, optimizer='sgd', l_rate=0.1, beta=.9):
-        # Hyperparameters
+              batch_size=64, l_rate=0.1):
+        # number of trainings
         self.epochs = epochs
+        # divide the data into batches
         self.batch_size = batch_size
         num_batches = -(-x_train.shape[0] // self.batch_size)
 
-        # Initialize optimizer
-        self.optimizer = optimizer
-
-        start_time = time.time()
         # Train
         for i in range(self.epochs):
             # Shuffle
-            permutation = np.random.permutation(x_train.shape[0])
-            x_train_shuffled = x_train[permutation]
-            y_train_shuffled = y_train[permutation]
+            shuffle = np.random.permutation(x_train.shape[0])
+            x_train_shuffled = x_train[shuffle]
+            y_train_shuffled = y_train[shuffle]
 
             for j in range(num_batches):
-                # Batch
+                # begin index of the batch
                 begin = j * self.batch_size
+                # end index of the batch
                 end = min(begin + self.batch_size, x_train.shape[0]-1)
+                
                 x = x_train_shuffled[begin:end]
                 y = y_train_shuffled[begin:end]
 
-                # Forward
+                # Backpropagation algorithm
+                # Forward pass
                 output = self.forward_pass(x)
-                # Backward
+                # Backward pass
                 self.backward_pass(y, l_rate)
 
             # Train data
             output = self.forward_pass(x_train)
-            train_accuracy = self.accuracy(y_train, output)
+            train_accuracy = self.get_accuracy(y_train, output)
             # Test data
             output = self.forward_pass(x_test)
-            test_acc = self.accuracy(y_test, output)
-            # print epoch, train accuracy, test accuracy
-            result = "Epoch {}: {:.2f}s, train acc={:.2f}, test acc={:.2f},"
-            print(result.format(i+1, time.time()-start_time,
-                                train_accuracy,
-                                test_acc))
+            test_acc = self.get_accuracy(y_test, output)
+            
+            result = "Epoch {}: train acc = {:.2f}, test acc = {:.2f},"
+            print(result.format(i+1, train_accuracy, test_acc))
 
 
 def preprocess_data():
@@ -174,9 +206,8 @@ def main():
 
     # Get the number of classes from the target variable
     num_classes = y_train.shape[1]
-    dnn = NeuralNetwork(16, 10, num_classes, activation='sigmoid')
-    dnn.train(X_train, y_train, X_test, y_test, batch_size=32,
-              optimizer='sgd', l_rate=0.1, beta=.9)
+    dnn = NeuralNetwork(16, 10, num_classes, activation='sigmoid', beta=0.9)
+    dnn.train(X_train, y_train, X_test, y_test, batch_size=32, l_rate=0.1)
 
 
 if __name__ == "__main__":
--- README.txt
+++ README.txt
@@ -1,15 +1,29 @@
 Name: Thien Nguyen
 NetID: DXN210021
 
-Name:
-NetID:
+Name: Trung "Jason" Nguyen
+NetID: ttn190009
 
 Assignment 1
 CS6375 - Machine Learnning
 
 This package including:
 	README.TXT
-	
+	Dry_Bean_Dataset.xlsx
+	NeuralNetwork.py
 
+
+How to run:
+	python3 NeuralNetwork.py
+	
+In the main code, we set test_size=0.25, which means 25% of the data will be used for testing, and the remaining 75% will be used for training.
+
+The reasoning behind this choice is as follows:
+
+Training Set Size: A larger training set (75% of the data) allows the model to learn more complex patterns and relationships in the data, which is particularly important for tasks with high-dimensional or complex data.
+
+Testing Set Size: A test set size of 25% is considered large enough to provide a reliable estimate of the model's performance on unseen data, while not being too small to lead to high variance in the performance estimates.
+
+Balance: The 75/25 split strikes a good balance between having a large training set for effective learning and a reasonably sized testing set for reliable performance evaluation.
 
 
